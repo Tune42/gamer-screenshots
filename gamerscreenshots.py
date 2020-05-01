08a11761ffd44d80d81ab32732767aa79f0e9e3f
Tune42 <59482059+Tune42@users.noreply.github.com>
2020-04-30 23:34:25 -0500

    embedded imgur links and made style changes

--- gamerscreenshots.py
+++ gamerscreenshots.py
@@ -15,7 +15,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='static/richLUL.png')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
@@ -39,10 +39,11 @@ class Post(db.Model):
 def home():
     posts = []
     users = {}
-    for post in Post.query.all():
+    for post in Post.query.order_by(Post.date_posted).all():
         posts.append(post)
+    posts.reverse()
     for user in User.query.all():
-        users[user.id] = user.username
+        users[user.id] = (user.username, user.image_file)
     return render_template('home.html', posts=posts, users=users)
 
 #about route
